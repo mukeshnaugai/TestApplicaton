34835ed459bd42f7e77e6e96a15d21d392bc1fee
mukesh <mmuki80@gmail.com>
2020-05-24 00:31:17 +0530

    changes

---
+++ app/src/main/java/com/e/mybroadcast/MainActivity.java
@@ -0,0 +1,22 @@
+package com.e.mybroadcast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+}
